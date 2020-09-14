74972ba83f41bf8dc73dbe339d05c6006d102e11
Fl_GUI <flor.guilini@hotmail.com>
2020-09-14 22:48:12 +0200

    Add test for valid and invalid making moves

--- code/logic/src/test/java/shogun_logic/BoardMovesTest.java
+++ code/logic/src/test/java/shogun_logic/BoardMovesTest.java
@@ -96,6 +96,71 @@ public class BoardMovesTest {
     assertTrue(b.isValidMove(3, 5, 2, 2));
   }
 
+  @Test public void testMakeValidMove() {
+    Board b;
+    Piece p;
+    int[] nums = new int[4];
+
+    b = BoardFactory.emptyBoard(8);
+    nums = getVariation(2);
+
+    p = new Piece(nums, false, Color.RED);
+
+    b.putPiece(p, 0, 0);
+    assertTrue(b.makeMove(0, 0, 1, 1));
+    assertTrue(b.makeMove(1, 1, 1, 3));
+    assertTrue(b.makeMove(1, 3, 3, 3));
+    assertTrue(b.makeMove(3, 3, 3, 1));
+  }
+
+  @Test public void testMakeInvalidMove() {
+    Board b;
+    Piece p1, p2;
+    int[] nums = new int[4];
+
+    b = BoardFactory.emptyBoard(8);
+    nums = getVariation(2);
+
+    p1 = new Piece(nums, false, Color.RED);
+    p2 = new Piece(nums, false, Color.RED);
+
+    b.putPiece(p1, 0, 0);
+    b.putPiece(p2, 1, 1);
+
+    assertFalse(b.makeMove(0, 0, 1, 1));
+    assertFalse(b.makeMove(0, 0, -1, 0));
+    assertFalse(b.makeMove(1, 1, 4, 1));
+    assertFalse(b.makeMove(1, 1, 0, 0));
+  }
+
+  @Test public void testMakeValidMoveTake() {
+    Board b;
+    Piece p1, p2;
+    int[] nums = new int[4];
+
+    b = BoardFactory.emptyBoard(8);
+    nums = getVariation(2);
+
+    p1 = new Piece(nums, false, Color.RED);
+    p2 = new Piece(nums, false, Color.WHITE);
+
+    b.putPiece(p1, 0, 0);
+    b.putPiece(p2, 1, 1);
+
+    assertTrue(b.makeMove(0, 0, 1, 1));
+    assertTrue(b.getPiece(0, 0).isEmpty());
+    assertTrue(b.getPiece(1, 1).isPresent());
+
+    b.putPiece(p2, 1, 3);
+    assertTrue(b.makeMove(1, 1, 1, 3));
+  
+    b.putPiece(p2, 2, 4);
+    assertTrue(b.makeMove(1, 3, 2, 4));
+
+    b.putPiece(p2, 4, 4);
+    assertTrue(b.makeMove(2, 4, 4, 4));
+  }
+
   private int[] getVariation(int steps) {
     int[] s = {steps,steps,steps,steps};
     return s;
